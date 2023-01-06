979e60b944918f4ea13e4b2fd4b479070edac31f
Iamjaeyoung <wodud6967@naver.com>
2023-01-06 20:53:38 +0900

    유저 고치기 및 포스트맨적용

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+const{ User } = require("./models/User");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://wodud:rosua112@cluster0.alktmxb.mongodb.net/?retryWrites=true&w=majority', {
   
@@ -9,6 +15,20 @@ mongoose.connect('mongodb+srv://wodud:rosua112@cluster0.alktmxb.mongodb.net/?ret
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+
+  //회원 가입 할떄 필요한 정보들은 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) =>{
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  });
 })
 
 app.listen(port, () => {
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose =require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type:String,
+        minlength: 5
+    },
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports ={ User }
